599fd0a1fbe39e6a96849ec245b09f579c5b1319
Santiago Borja Romero <santiago.borja@gmail.com>
2024-09-08 22:36:44 -0500

    revision interface

--- web/src/app/interfaces/producto.interface.ts
+++ web/src/app/interfaces/producto.interface.ts
@@ -1,20 +1,20 @@
 export interface ProductoInterface {
     idProductos: number; 
+    idIVA?: number;
+    idUnidad_Medida?: number;
+    idProveedores?: number;
+    Unidad_Medida?: string;
+    IVA_Detalle?: string;
+    idKardex?: number;
+    IVA_idIVA?: number;
     Codigo_Barras: string;
     Nombre_Producto: string;
     Graba_IVA: number;
     Cantidad?: number;
     Valor_Compra?: number;
     Valor_Venta?: number;
-    idUnidad_Medida?: number;
-    idIVA?: number;
-    idProveedores?: number;
-    Unidad_Medida?: string;
-    IVA_Detalle?: string;
     Fecha_Transaccion?: string;
     Tipo_Transaccion?: string;
-    idKardex?: number;
-    IVA_idIVA?: number;
     Unidad_Medida_idUnidad_Medida?: number;
     Proveedores_idProveedores?: number;
 }
